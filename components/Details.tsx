891d11f83fea358a2c6a46e22bfc4aed976fe1e2
Noe Aguilar <noeaguilar@auctiontechnologygroup.com>
2025-10-07 08:34:46 -0600

    main: 🛠  additional info added 🐙

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,12 +1,18 @@
 export const Details = () => {
   return (
-    <div className="flex flex-col justify-between h-[100px]">
-      <span>Card Dimensions: 380px by 479px</span>
-      <span>Card Shadow Color: rgba(0, 0, 0, 0.1)</span>
-      <span>Card Shadow Hover Color: rgba(0, 0, 0, 0.26)</span>
-      <span>Card Picture Dimentions: 180px diamater</span>
-      <span>Button/Select Dimentions: 180px by 60px</span>
-      <span>Button disabled color: #aaaaaa</span>
-    </div>
+    <article className="flex flex-row justify-around">
+      <section className="flex flex-col">
+        <span>Card Dimensions: 380px by 479px</span>
+        <span>Card Shadow Color: rgba(0, 0, 0, 0.1)</span>
+        <span>Card Shadow Hover Color: rgba(0, 0, 0, 0.26)</span>
+        <span>Card Picture Dimentions: 180px diamater</span>
+        <span>Button/Select Dimentions: 180px by 60px</span>
+        <span>Button disabled color: #aaaaaa</span>
+      </section>
+      <section className="flex flex-col">
+        <a href="https://codesandbox.io/" target="_blank"> Code Sandbox </a>
+        <a href="https://randomuser.me/api/?page=1&results=6" target="_blank"> Random User API </a>
+      </section>
+    </article>
   );
 };
